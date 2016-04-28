b3167b19264289bd9e68f70ffff64f59eeb0e6be
Arron Atchison <arron.atchison@gmail.com>
2016-04-28 15:55:48 -0500

    added compile-avrdude.sh and changed ./install.sh path to refleect that

---
+++ compile-avrdude.sh
@@ -0,0 +1,8 @@
+sudo apt-get install bison flex libusb-dev
+cd /tmp
+wget http://download.savannah.gnu.org/releases/avrdude/avrdude-6.1.tar.gz
+tar xf avrdude-6.1.tar.gz
+cd avrdude-6.1
+./configure --prefix=/opt/avrdude --enable-linuxgpio
+make
+sudo make install
--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo avrdude -p atmega328p -C avrdude-gpio.conf -c linuxgpio -v -U flash:w:enclosure.hex
+sudo /opt/avrdude/bin/avrdude -p atmega328p -C avrdude-gpio.conf -c linuxgpio -v -U flash:w:enclosure.hex
