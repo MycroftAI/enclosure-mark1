02d1372941af0ca8fe3cf0ae56e357ad547b9e13
Jonathan D'Orleans <jonathan.dorleans@gmail.com>
2016-05-05 14:19:50 -0400

    Removing sudo and installation dependencies from scripts

--- compile.sh
+++ compile.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-./install-arduino.sh
 ARDUINO_SDK_PATH=/opt/arduino
 mkdir -p build
 ${ARDUINO_SDK_PATH}/arduino-builder -dump-prefs -logger=machine -hardware "${ARDUINO_SDK_PATH}/hardware" -tools "${ARDUINO_SDK_PATH}/tools-builder" -tools "${ARDUINO_SDK_PATH}/hardware/tools/avr" -built-in-libraries "${ARDUINO_SDK_PATH}/libraries" -libraries "./lib" -fqbn=arduino:avr:pro:cpu=16MHzatmega328 -ide-version=10608 -build-path "${PWD}/build" -warnings=none -prefs=build.warn_data_percentage=75 -verbose "${PWD}/src/enclosure.ino"
--- install-arduino.sh
+++ install-arduino.sh
@@ -4,6 +4,6 @@ set -Ee
 if [ ! -d '/opt/arduino' ]; then
     cd /tmp
     wget 'http://path/to/arduino/package'
-    sudo tar -xf 'arduino-1.6.8-linux64.tar.xz' -C '/opt'
-    sudo mv 'arduino-1.6.8' 'arduino'
+    tar -xf 'arduino-1.6.8-linux64.tar.xz' -C '/opt'
+    mv 'arduino-1.6.8' 'arduino'
 fi
