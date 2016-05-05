c6819147c09ca3726850024be9c8eac92a9843f7
Jonathan D'Orleans <jonathan.dorleans@gmail.com>
2016-05-05 14:01:31 -0400

    Adding run command to publish.sh

--- publish.sh
+++ publish.sh
@@ -4,6 +4,14 @@
 cp build/enclosure.ino.hex enclosure.ino.hex
 tar -czvf enclosure.tar.gz enclosure.ino.hex upload.sh install-avrdude.sh avrdude-gpio.conf
 
+function _run() {
+  if [[ "$QUIET" ]]; then
+    echo "$*"
+  else
+    eval "$@"
+  fi
+}
+
 S3_HOST=s3://bootstrap.mycroft.ai/artifacts
 _run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public . ${S3_HOST}/amd64/latest/
 _run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public enclosure.tar.gz ${S3_HOST}/amd64/latest
