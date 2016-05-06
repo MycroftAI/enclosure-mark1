62d590f9cf7315195aeb80a7188c88ce4f85b3c9
aatchison <arron.atchison@gmail.com>
2016-05-06 13:59:10 -0500

    third times the charm

--- publish.sh
+++ publish.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-./compile.sh
 
-cp build/enclosure.ino.hex enclosure.ino.hex
-tar -czvf enclosure.tar.gz enclosure.ino.hex upload.sh install-avrdude.sh avrdude-gpio.conf
+#./compile.sh
 
-function _run() {
+#cp build/enclosure.ino.hex enclosure.ino.hex
+#tar -czvf $MYCROFT_ENCLOSURE_ARTIFACT_DIR/$ARTIFACT_BASE.tar.gz enclosure.ino.hex upload.sh install-avrdude.sh avrdude-gpio.conf
+
+function _run_s3_upload() {
   if [[ "$QUIET" ]]; then
     echo "$*"
   else
@@ -12,10 +13,47 @@ function _run() {
   fi
 }
 
-S3_HOST=s3://bootstrap.mycroft.ai/artifacts
-_run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public enclosure.tar.gz ${S3_HOST}/amd64/latest/enclosure.tar.gz
-_run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public enclosure.tar.gz ${S3_HOST}/amd64/latest/enclosure.tar.gz
 
-_run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public enclosure.tar.gz ${S3_HOST}/armhf/latest/enclosure.tar.gz
-_run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public enclosure.tar.gz ${S3_HOST}/armhf/latest/enclosure.tar.gz
+function _init_vars() {
+  TOP='.'
+  PROJECT_NAME="enclosure"
+  VERSION="$(basename $(git describe --abbrev=0 --tags) | sed -e 's/v//g')"
+  ARCH="armhf"
+  ARTIFACT_BASE="mycroft-enclosure-standalone-${ARCH}-${VERSION}"
+  MYCROFT_ENCLOSURE_ARTIFACT_DIR=${TOP}/build_artifacts/${ARTIFACT_BASE}
+    
+  S3_HOST=s3://bootstrap.mycroft.ai/artifacts
+
+}
+
+function _compile_arduino_sketch() {
+  ./compile.sh
+}
+
+
+function _create_tarball() {
+  mkdir -pv ${MYCROFT_ENCLOSURE_ARTIFACT_DIR}
+  tar -czvf $MYCROFT_ENCLOSURE_ARTIFACT_DIR/${ARTIFACT_BASE}-1.tar.gz enclosure.ino.hex upload.sh install-avrdude.sh avrdude-gpio.conf
+}
+
+function _upload() {
+  echo "${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public ${MYCROFT_ENCLOSURE_ARTIFACT_DIR}/${ARTIFACT_BASE}-1.tar.gz ${S3_HOST}/${ARCH}/${PROJECT_NAME}/${VERSION}/"
+  #_run_s3_upload s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public enclosure.tar.gz ${S3_HOST}/amd64/latest/enclosure.tar.gz
+  #_run_s3_upload s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public enclosure.tar.gz ${S3_HOST}/amd64/latest/enclosure.tar.gz
+
+  #_run_s3_upload s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public enclosure.tar.gz ${S3_HOST}/armhf/latest/enclosure.tar.gz
+  #_run_s3_upload s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public enclosure.tar.gz ${S3_HOST}/armhf/latest/enclosure.tar.gz 
+
+}
+
+_init_vars
+_compile_arduino_sketch
+_create_tarball
+_upload
+
+echo "version=\"${VERSION}\""
+echo $ARCH
+echo $ARTIFACT_BASE
+
+
 
