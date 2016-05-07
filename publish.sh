89cb4cc1fdab1e13a4f88c300b2a9431095d2b7e
aatchison <arron.atchison@gmail.com>
2016-05-06 19:08:14 -0500

    removed _ from functions; including version.txt in tar archive

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function _init_vars() {
+function init_vars() {
   TOP='.'
   PROJECT_NAME="enclosure"
   VERSION="$(basename $(git describe --abbrev=0 --tags) | sed -e 's/v//g')"
@@ -10,23 +10,24 @@ function _init_vars() {
   S3_HOST=s3://bootstrap.mycroft.ai/artifacts
 }
 
-function _compile_arduino_sketch() {
+function compile_arduino_sketch() {
   ./compile.sh
 }
 
 
-function _create_tarball() {
+function create_tarball() {
   mkdir -pv ${ARTIFACT_DIR}
+  echo $VERSION > ${TOP}/version.txt
   echo $VERSION > ${ARTIFACT_DIR}/latest
-  tar -czvf $ARTIFACT_DIR/${ARTIFACT_BASE}.tar.gz enclosure.ino.hex upload.sh install-avrdude.sh avrdude-gpio.conf
+  tar -czvf $ARTIFACT_DIR/${ARTIFACT_BASE}.tar.gz enclosure.ino.hex upload.sh install-avrdude.sh avrdude-gpio.conf version.txt
 }
 
-function _upload() {
-  _run_s3 s3cmd -c  ${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public ${ARTIFACT_DIR}/${ARTIFACT_BASE}.tar.gz ${S3_HOST}/${ARCH}/${PROJECT_NAME}/${VERSION}/
-  _run_s3 s3cmd -c  ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public ${ARTIFACT_DIR}/latest ${S3_HOST}/${ARCH}/${PROJECT_NAME}/latest
+function upload() {
+  _run s3cmd -c  ${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public ${ARTIFACT_DIR}/${ARTIFACT_BASE}.tar.gz ${S3_HOST}/${ARCH}/${PROJECT_NAME}/${VERSION}/
+  _run s3cmd -c  ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public ${ARTIFACT_DIR}/latest ${S3_HOST}/${ARCH}/${PROJECT_NAME}/latest
 }
 
-function _run_s3() {
+function run_s3() {
   if [[ "$QUIET" ]]; then
     echo "$*"
   else
@@ -34,7 +35,7 @@ function _run_s3() {
   fi
 }
 
-_init_vars
-_compile_arduino_sketch
-_create_tarball
-_upload
+init_vars
+compile_arduino_sketch
+create_tarball
+upload
