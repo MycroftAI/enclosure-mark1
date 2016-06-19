974ba748b4bd49d2033e66dae6c72f80b855a4ee
aatchison <arron.atchison@gmail.com>
2016-06-19 14:50:17 -0500

    updated publish and upload script to reflect correct paths

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo /opt/avrdude/bin/avrdude -p atmega328p -C avrdude-gpio.conf -c linuxgpio -v -U flash:w:/opt/enclosure/enclosure.ino.hex
+sudo /opt/avrdude/bin/avrdude -p atmega328p -C avrdude-gpio.conf -c linuxgpio -v -U flash:w:/opt/enclosure/build/enclosure.ino.hex
