29f13cf0198208ba8f4ac11bfdd5bc084c31bbb0
Isaac Ward <isaacnward@gmail.com>
2016-07-08 15:07:53 -0500

    moved position calculation to helper methods

--- lib/MycroftWeather/MycroftWeather.cpp
+++ lib/MycroftWeather/MycroftWeather.cpp
@@ -5,14 +5,12 @@ MycroftWeather::MycroftWeather(MycroftMouth* mouth, MycroftEyes* eyes) {
     this->eyes = eyes;
 }
 
-void MycroftWeather::display(int8_t condition, String temperature){
+void MycroftWeather::calculateImagePosition(String temperature){
     char first = temperature.charAt(0);
     if(temperature.length() == 1){
       imgPos = 7;
-      tempPos = 18;
     }
     else if (temperature.length() == 2){
-      tempPos = 16;
       if (first == '-'){
         imgPos = 6;
       }
@@ -22,13 +20,30 @@ void MycroftWeather::display(int8_t condition, String temperature){
     }
     else if (temperature.length() == 3){
       imgPos = 3;
-      if (first == '-'){
-        tempPos = 13;
-      }
-      else{
-        tempPos = 14;
-      }
     }
+}
+
+void MycroftWeather::calculateTemperaturePosition(String temperature){
+    char first = temperature.charAt(0);
+    if(temperature.length() == 1){
+        tempPos = 18;
+    }
+    else if (temperature.length() == 2){
+        tempPos = 16;
+    }
+    else if (temperature.length() == 3){
+        if (first == '-'){
+            tempPos = 13;
+        }
+        else{
+            tempPos = 14;
+        }
+    }
+}
+
+void MycroftWeather::display(int8_t condition, String temperature){
+    calculateImagePosition(temperature);
+    calculateTemperaturePosition(temperature);
     this->mouth->reset();
     this->mouth->drawImage(imgPos, condition, WEATHER_CONDS);
     temperature += "\\";
--- lib/MycroftWeather/MycroftWeather.h
+++ lib/MycroftWeather/MycroftWeather.h
@@ -6,7 +6,7 @@ class MycroftWeather
 public:
   MycroftWeather(MycroftMouth *mouth, MycroftEyes *eyes);
 
-  void display(int8_t condition, String current);
+  void display(int8_t condition, String temperature);
 
 private:
   MycroftMouth *mouth;
@@ -14,4 +14,8 @@ private:
   MycroftEyes *eyes;
 
   char imgPos, tempPos;
+
+  void calculateImagePosition(String temperature);
+
+  void calculateTemperaturePosition(String temperature);
 };
