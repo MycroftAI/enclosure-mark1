8715b4574b0102d81ddc366dfaec01b7ac28365b
Wolfgange3311999 <matthew3311999@gmail.com>
2016-07-18 11:18:45 -0500

    Created MycroftDisplay class

---
+++ lib/MycroftDisplay/MycroftDisplay.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "MycroftHT1632.h"
+
+#include "font_5x4.h"
+#include "font_8x4.h"
+
+class MycroftDisplay {
+public:
+	MycroftDisplay(int pinCS1, int pinWR, int pinDATA);
+	void drawFrame(byte index, const char (*IMG)[16]);
+	void drawIcon(byte pos, byte index, const char (*ICONS)[16]);
+	void drawText(String text, int8_t pos, bool small);
+	void slideText();
+	void clear();
+	void render();
+
+private:
+	void draw8x8(byte pos, byte index, const char (*IMG)[16]);
+	void readBuffer(byte idx, const char (*IMG)[16]);
+
+	MycroftHT1632 ht1632;
+	char currentImage[16];
+};
