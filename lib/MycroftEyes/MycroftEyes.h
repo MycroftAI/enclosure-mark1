fdfc104b6933faa46e78d89caba6995491d806a6
Arron Atchison <arron.atchison@gmail.com>
2016-08-16 09:54:33 -0500

    Revert "Issues/#9: Volume display on eyes"

--- lib/MycroftEyes/MycroftEyes.h
+++ lib/MycroftEyes/MycroftEyes.h
@@ -1,21 +1,16 @@
 #pragma once
 
 #include "Adafruit_NeoPixel.h"
-#include "EyeFrames.h"
 
 class MycroftEyes {
 public:
 	enum Side {
-		BOTH, LEFT, RIGHT, UP, DOWN, CROSS
+		BOTH, LEFT, RIGHT
 	};
 
 	Adafruit_NeoPixel neoPixel;
 
 	MycroftEyes(uint16_t size, uint8_t pin, uint16_t type);
-
-	enum Animation {
-		BLINK, NARROW, LOOK, UNLOOK, WIDEN, VOLUME, NONE
-	};
 
 	void setup();
 
@@ -23,11 +18,11 @@ public:
 
 	void off();
 
-	void startAnim(Animation anim, const char side);
+	void blink(unsigned long wait, const char side);
 
-	void setEyePixels(const char side, uint8_t pixels);
+	void narrow(unsigned long wait, const char side);
 
-	void updateAnimation();
+	void look(unsigned long wait, const char side);
 
 	void updateColor(uint8_t r, uint8_t g, uint8_t b);
 
@@ -38,53 +33,17 @@ public:
 	void set(uint32_t color);
 
 private:
-	uint32_t color, c;
-
-	uint16_t r1, r2, ro1, ro2;
-
-	uint8_t volumePix;
-
-	unsigned long nextTime;
-
-	Side currentSide;
-
-	enum State {
-		OPEN, LOOKING, NARROWED, CLOSED, ANIMATING
-	};
-
-	State currentState;
-
-	Animation currentAnim, queuedAnim;
-
-	char queuedSide, lookSide;
-
-	boolean isQueued;
-
-	byte MAX, pos, opJump, steps, leftJump, i, j, update;
-
-	unsigned long delayTime;
+	uint32_t color;
 
 	static bool leftOn(Side side);
 
 	static bool rightOn(Side side);
 
-	void setEyePixels(uint8_t pixels);
+	void blink(Side side, byte pos, byte leftJump, unsigned long wait);
 
-	void animSetup(Animation anim, const char side);
+	void narrow(Side side, byte pos, byte leftJump, unsigned long wait);
 
-	void runAnim();
-
-	void startTransition(Animation transition, Animation anim, const char side);
-
-	void updateCounters();
-
-	void setSide(const char side);
-
-	void setVars();
-
-	void resetCounters();
-
-	void eyesNarrow(uint32_t c, int wait);
+	void look(byte pos, byte leftJump, unsigned long wait);
 
 	uint16_t mod(long a, long b);
 };
