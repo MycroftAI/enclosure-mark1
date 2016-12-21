1f430bfed2d3b809a664230174758f33620a2554
penrods <github@gpll.org>
2016-12-21 16:06:09 -0600

    This is related to issue #70
    The upload.sh script has been changed in several ways:
    - When run by default, it will now stop and restart the mycroft-enclosure-client service (use -s or -skipService to skip this behavior)
    - When run by default, the upload will not occur if not needed (see below for what that means).  Use -f or -force to force an upload.
    - After a successful update, the Arduino is queried over the serial line for its version number, and that self-report version is stored in the file /opt/enclosure/build/version.txt
    
    The upload.sh script calls verifyArduino.py to check version info.  If there is a version.txt file, the value is compared from the self-reported version the Arduino responds with across the serial line.  However, if the .hex is newer (file mode date) than the stored version.txt it is considered invalid.
    
    TODO: This code is based on the system.ping responding with a version number.  It needs to be updated to operate against the newly implemented system.version command of #69.

---
+++ verifyArduino.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+""" Verify the loaded Arduino code version
+
+This script determines if a Mycroft Mark 1 device needs to upload
+new code to the connected Arduino.  Return codes:
+  1  needs to be updated (old or not loaded)
+  0  already loaded with the latest version
+< 0  error code, don't attempt to update
+
+Usage: python verifyArduino [-savever]
+       -savever   when specified, the build/version.txt is recreated based
+                on the Arduino's response
+"""
+
+import sys
+import serial
+import time
+import os.path
+from mycroft.configuration import ConfigurationManager
+
+pathVersion = "/opt/enclosure/build/version.txt"
+pathSketch = "/opt/enclosure/build/enclosure.ino.hex"
+
+if not os.path.isfile(pathSketch):
+    sys.exit(-1)  # no Arduino Sketch to upload!
+
+# Read the platform.  This only works with Mycroft Mark 1
+config = ConfigurationManager.get().get("enclosure")
+platform = config.get('platform')
+if not platform == "mycroft_mark_1":
+    print "Not a Mark 1 (" + str(platform) + ")"
+    sys.exit(-2)  # not Mark 1, so no Arduino to talk to!
+
+
+# Attempt to open the serial port connected to the Arduino
+port = "Undefined"
+try:
+    port = config.get("port")
+    rate = int(config.get("rate"))
+    timeout = int(config.get("timeout"))
+    tty = serial.serial_for_url(
+        url=port, baudrate=rate, timeout=timeout)
+    print ("Connected to: " + port + " rate: " + str(rate) +
+            " timeout: " + str(timeout))
+except:
+    print "It is not possible to connect to serial port: " + port
+    sys.exit(-3)  # don't try to update
+    raise
+
+
+# Now see if there is a valid "current version" for the Arudino sketch we
+# have.  If not, we'll go ahead and upload and generate this later...
+latestVersion = None
+if os.path.isfile(pathVersion):
+    # Only trust the version.txt if it was created after the Sketch,
+    # otherwise consider it outdated and ignore it.
+    if os.path.getmtime(pathVersion) > os.path.getmtime(pathSketch):
+        with open(pathVersion, 'r') as myfile:
+            latestVersion = myfile.read()
+
+# Attempt to talk to the Arduino over the serial port get loaded Sketch
+# version and react accordingly.
+#
+try:
+    # Thoroughly flush the serial port.
+    #xxx # Since this happens right after boot, sometimes there is junk
+    #xxx # on the serial port line from electrical noise on a Pi, so sleep.
+    #xxx # time.sleep(1)
+    tty.flushInput()
+    tty.flushOutput()
+
+    # Write "system.ping"
+    print "Pinging..."
+    tty.write("system.ping")
+    time.sleep(0.1)
+
+    # Now check to see if we got a response from the ping command
+    # command we just sent.  Remember, there might not be a Mark 1
+    # Arduino on the other side of the serial port.
+    resp = tty.readline().rstrip()
+    print "Reply: " + resp
+    if "Command: system.ping" in resp:
+        # Got Arduino ping response, attempting to extract version
+        resp = tty.readline().rstrip()
+        print "Reply: " + resp
+        if "Version:" in resp:
+            ver = tty.readline().rstrip()
+            print "Reply: " + ver
+
+            # Save the version for subsequent testing
+            if len(sys.argv) > 1 and sys.argv[1] == "--savever":
+                with open(pathVersion, "w") as text_file:
+                    text_file.write(ver)
+                print "Version reported from Arduino saved"
+                sys.exit(0)
+
+            if latestVersion == ver:
+                print "Arduino up to date!"
+                sys.exit(0)
+            else:
+                print "Arduino needs to be updated"
+                sys.exit(1)
+    else:
+        print "Arduino not responding, assuming it needs to be updated"
+        sys.exit(1)
+
+finally:
+    tty.close()
+
+
+sys.exit(-3)  # unknown state, don't try to update
