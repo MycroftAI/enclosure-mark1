5e7b4cc5976bf9773c01d823fe638d1016814735
Austin Juhl <austin.juhl@mycroft.ai>
2017-07-14 11:23:08 -0500

    Added apache 2.0 license to all *.sh, *.py, *.cpp, *.h files without existing licenses.

--- verifyArduino.py
+++ verifyArduino.py
@@ -1,3 +1,18 @@
+#########################################################################
+#Copyright 2017, Mycroft AI, Inc.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+#########################################################################
 # This script determines if a Mycroft Mark 1 device needs to
 # upload new code to the connected Arduino.  Return codes:
 #   1  needs to update
