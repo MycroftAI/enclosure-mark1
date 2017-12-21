ad7c6930c5471020e1cc1cef949c2cdaa2c9af53
Michael Nguyen <ppnguyen91@gmail.com>
2017-12-20 18:30:02 -0600

    Check if platform is mark 1 on upload.sh (#93)
    Check if the platform is mark 1 on upload.sh and exit if it's not. This will prevent enclosures like picroft to try to upload the mark 1 Arduino code which causes a spike in CPU

--- upload.sh
+++ upload.sh
@@ -15,6 +15,8 @@
 #########################################################################
 #!/usr/bin/env bash
 
+if ! grep -qF "mycroft_mark_1" /etc/mycroft/mycroft.conf; then exit; fi
+
 # Default script behavior
 STOPSERVICE=1   # auto stop and restart mycroft-enclosure-client
 UPDATE=0        # only upload when version info indicates necessary
